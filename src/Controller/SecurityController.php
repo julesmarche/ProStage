371454b87d937b6de6287ee1e80e06fddfef4f4d
Marche Jules <jmarche001@iutbayonne.univ-pau.fr>
2022-03-11 15:15:21 +0100

    Formulaire de login et de deconnexion

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/deconnexion", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+      /**
+     * @Route("/inscription", name="app_register")
+     */
+    public function register(Request $requeteHttp, EntityManagerInterface $manager) 
+    {
+        $user = new User();
+
+        $formulaireUtilisateur = $this->createForm(UserType::class,$user);
+
+        $formulaireUtilisateur -> handleRequest ( $requeteHttp );
+
+
+        if($formulaireUtilisateur->isSubmitted()&&$formulaireUtilisateur->isValid())
+        {
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+
+
+        return $this->render('security/register.html.twig',['formulaireInscription' => $formulaireUtilisateur -> createView ()
+        ]
+        );
+    }
+}
